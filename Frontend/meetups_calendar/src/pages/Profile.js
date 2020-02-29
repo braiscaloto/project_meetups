0f13936fd92ae5e141a339368a571621f095237e
braiscaloto <braiscaloto92@gmail.com>
2020-02-29 17:32:46 +0100

    sincronizado ramas

--- Frontend/meetups_calendar/src/pages/Profile.js
+++ Frontend/meetups_calendar/src/pages/Profile.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AvatarContainer } from '../components/AvatarContainer';
-=======
-import React from "react";
-import { Link } from "react-router-dom";
-import jwt_decode from "jwt-decode";
->>>>>>> afcc966e89f96aaf76c15574f161c7d059f2e33d
 
 export function Profile() {
-  const user = JSON.parse(localStorage.getItem("currentUser"));
+  const user = JSON.parse(localStorage.getItem('currentUser'));
 
   return (
     <React.Fragment>
-<<<<<<< HEAD
       <main className='profile'>
-        <Link className='btn' to={`/calendar`}>
+        <Link className='btn' to={`/`}>
           GO CALENDAR
         </Link>
 
@@ -26,38 +19,14 @@ export function Profile() {
         </Link>
         <div className='contactProfile'>
           <p className='emailProfile'>Email: {user.email}</p>
-=======
-      <main className="profile">
-        <Link className="btn" to={`/CalendarPrivate`}>
-          GO CALENDAR
-        </Link>
-        <h1 className="titleProfile">{user.name}</h1>
-
-        <div className="avatarContainer">
-          {user.avatarUrl !== null && (
-            <img className="avatarProfile" src={user.avatarUrl} />
-          )}
-          {user.avatarUrl === null && (
-            <img
-              className="avatarProfile"
-              src={require("../images/default-avatar.png")}
-            />
-          )}
-        </div>
-        <Link className="btn" to={`/update/${user.userId}`}>
-          Update Profile
-        </Link>
-        <div className="contactProfile">
-          <p className="emailProfile">Contact me: {user.email}</p>
->>>>>>> afcc966e89f96aaf76c15574f161c7d059f2e33d
         </div>
 
-        <Link className="btn" to="/">
+        <Link className='btn' to='/'>
           <button
-            className="logoutButton"
+            className='logoutButton'
             onClick={() => {
               localStorage.clear();
-              window.location.href = "/";
+              window.location.href = '/';
             }}
           >
             LOGOUT
